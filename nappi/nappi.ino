de42d92492006526f76e78f8b7995a69ce94a38a
anakuron <antti.nissinen@gmail.com>
2021-03-18 23:15:37 +0200

    added neopixel led to button

--- nappi/nappi.ino
+++ nappi/nappi.ino
@@ -1,3 +1,16 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN 3 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 1 // Popular NeoPixel ring size
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+
 #include "Keyboard.h"
  
 #define PTT 240
@@ -12,12 +25,15 @@ void setup(){
  
    //setup keyboard
    Keyboard.begin();
+
+   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
  
 //two states
 #define WAITING 0
 #define PRESSED 1
 void loop(){
+   pixels.clear(); // Set all pixel colors to 'off'
    static char state = WAITING; //state machine state
    static char debounce = 0; //counter for debounce
  
@@ -26,6 +42,8 @@ void loop(){
       case WAITING:
          //wait for button to be pressed, keep debounce cleared
          if (digitalRead(BUTTON) == HIGH){
+            pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+            pixels.show();   // Send the updated pixel colors to the hardware.
             debounce = 0;
             break;
          }
@@ -37,9 +55,11 @@ void loop(){
  
          //press key
          Keyboard.press(PTT);
- 
+        
          //change state
          state = PRESSED;
+         pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+         pixels.show();
          break;
  
       case PRESSED:
@@ -64,4 +84,4 @@ void loop(){
  
    //wait for next tick
    delay(1000/TICK);
-}   
+}
